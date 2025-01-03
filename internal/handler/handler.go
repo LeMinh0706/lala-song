9d09ce352106af8122a276c62adc8c3847e0b8fa
LeMinh0706 <leminhken124356@gmail.com>
2025-01-03 23:51:39 +0700

    something went wrong

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -9,39 +9,38 @@ import (
 )
 
 func ValidateRegister(f *fiber.Ctx, err error) error {
-	if validate, ok := err.(validator.ValidationErrors); ok {
-		for _, vali := range validate {
-			switch vali.Tag() {
-			case "min":
-				if vali.Field() == "Username" {
-					return res.ErrorResponse(f, 40008)
-				} else if vali.Field() == "Gender" {
-					return res.ErrorResponse(f, 40007)
-				} else if vali.Field() == "Password" {
-					return res.ErrorResponse(f, 40009)
-				} else if vali.Field() == "Fullname" {
-					return res.ErrorResponse(f, 40010)
-				}
-			case "max":
-				if vali.Field() == "Username" {
-					return res.ErrorResponse(f, 40008)
-				} else if vali.Field() == "Gender" {
-					return res.ErrorResponse(f, 40007)
-				}
-			case "required":
-				if vali.Field() == "Username" {
-					return res.ErrorResponse(f, 40008)
-				} else if vali.Field() == "Gender" {
-					return res.ErrorResponse(f, 40007)
-				} else if vali.Field() == "Password" {
-					return res.ErrorResponse(f, 40009)
-				} else if vali.Field() == "Fullname" {
-					return res.ErrorResponse(f, 40010)
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		errorMap := map[string]map[string]int{
+			"Username": {
+				"min":      40008,
+				"max":      40008,
+				"required": 40008,
+			},
+			"Password": {
+				"min":      40009,
+				"required": 40009,
+			},
+			"Fullname": {
+				"min":      40010,
+				"required": 40010,
+			},
+			"Gender": {
+				"min":      40007,
+				"max":      40007,
+				"required": 40007,
+			},
+		}
+
+		for _, vali := range validationErrors {
+			if fieldErrors, ok := errorMap[vali.Field()]; ok {
+				if errorCode, ok := fieldErrors[vali.Tag()]; ok {
+					return res.ErrorResponse(f, errorCode)
 				}
 			}
 		}
 	}
-	return res.ErrorResponse(f, 40000)
+
+	return res.ErrorNonKnow(f, err.Error())
 }
 
 func CheckQuery(f *fiber.Ctx, pageStr, pageSizeStr string) (int32, int32, int) {
